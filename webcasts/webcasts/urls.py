709ddd054e8eb38a451b9ef7373849126bef5baa
osaaxe-1 <oscar.axelstrom@hotmail.se>
2024-11-21 15:08:45 +0100

    Revert "Examples of models, admin, views, ..."
    This reverts commit 74068a63b38081f9b7f1610d6cd3e92a9a6062fa.

--- webcasts/webcasts/urls.py
+++ webcasts/webcasts/urls.py
@@ -16,16 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
-# Example patterns
-#urlpatterns = [
-#    path('courses/', views.CourseList.as_view(), name='course_list'),
-#    path('courses/<slug:slug>/', views.CourseDetail.as_view(), name='course_detail'),
-#    path('courses/<slug:course>/classes/', views.CourseClassList.as_view(), name='course_class_list'),
-#    path('classes/<str:slug>/', views.CourseClassDetail.as_view(), name='course_class_detail'),
-#]
